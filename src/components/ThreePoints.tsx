9aef5a7020332ee3e7e845b845b5dac5ff049ea7
Derick <derick@futurewonder.com>
2026-07-07 19:02:56 -0400

    change back the next.config

---
+++ src/components/ThreePoints.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export default function ThreePoints() {
+  return (
+    <div className="relative">
+      <div>
+        Point 1
+      </div>
+      <div>
+        Point 2
+      </div>
+      <div>
+        Point 3
+      </div>
+    </div>
+  );
+}
