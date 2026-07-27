ed084e3c753792b4463c0f846a5526c0d1b982b4
Derick <derick@futurewonder.com>
2026-07-26 22:04:22 -0400

    minor spelling corrections

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -3,7 +3,7 @@ export default function Quote() {
     <div className="bg-white p-4 md:p-16">
       <div className="max-w-300 mx-auto bg-white p-6 md:p-24 shadow-[0_0_24px_rgba(0,0,0,0.33)] text-lg md:text-xl">
         <div className="text-center text-black">
-          The MIC 30th Anniversary Campaign is an oppertunity to build a strongder student community. foster connections with other schools through shared events and performances, promote unity, and bring together students from across these communities," says Dr. Tim Phares, Principal of New Castle High School. of Carmel High School.  "This mileston provices a meaninful opopertunity to reflect on the conference's long-standing legacy of excellence, competetition, and sportsmanship, as well as the lasting impact it has had on generations of student-athletes and school communities.
+          The MIC 30th Anniversary Campaign is an opportunity to build a stronger student community, foster connections with other schools through shared events and performances, promote unity, and bring together students from across these communities," says Dr. Tim Phares, Principal of New Castle High School. of Carmel High School. "This milestone provides a meaningful opportunity to reflect on the conference's long-standing legacy of excellence, competition, and sportsmanship, as well as the lasting impact it has had on generations of student-athletes and school communities.
         </div>
         <div className="text-center mt-4 text-mic-blue font-bold">
           Dr. Tim Phares, Principal of Carmel High School.
